279e79e461ab2eaff5f306fd96af747dc68829d5
Josh Wilson <joshua.wilson@partnerspersonnel.com>
2023-06-27 16:42:03 -0600

    Change app.js to hold form placeholders

---
+++ order-form/src/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Form = () => {
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
