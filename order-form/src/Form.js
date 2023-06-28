667bfea1af20c3d89d926be83c19129f87c19104
Josh Wilson <joshua.wilson@partnerspersonnel.com>
2023-06-28 11:22:39 -0600

    Add more form inputs, upload partners logo

--- order-form/src/Form.js
+++ order-form/src/Form.js
@@ -4,15 +4,60 @@ import React from 'react';
 const Form = () => {
   return (
     <form>
-      <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+      <div className="form-group row align-items-center">
+        <label htmlFor="name" className="col-sm-auto col-form-label">Company</label>
+        <div className="col-sm">
+          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+        </div>
       </div>
-      <div className="form-group">
-        <label htmlFor="email">Email address</label>
-        <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+      <div className="form-group row align-items-center">
+        <label htmlFor="name" className="col-sm-auto col-form-label">Address</label>
+        <div className="col-sm">
+          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+        </div>
       </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
+      <div className="form-group row align-items-center">
+        <label htmlFor="name" className="col-sm-auto col-form-label">First Name</label>
+        <div className="col-sm">
+          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+        </div>
+      </div>
+      <div className="form-group row align-items-center">
+        <label htmlFor="name" className="col-sm-auto col-form-label">Last Name</label>
+        <div className="col-sm">
+          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+        </div>
+      </div>
+      <div className="form-group row align-items-center">
+        <label htmlFor="email" className="col-sm-auto col-form-label">Company email address</label>
+        <div className="col-sm">
+          <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+        </div>
+      </div>
+      <div className="form-group row align-items-center">
+        <label htmlFor="email" className="col-sm-auto col-form-label">Phone</label>
+        <div className="col-sm">
+          <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+        </div>
+      </div>
+      <div className="form-group row align-items-center">
+        <label htmlFor="email" className="col-sm-auto col-form-label">Local Branch</label>
+        <div className="col-sm">
+          <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+        </div>
+      </div>
+      <hr className="my-4" />
+      <div className="form-group row align-items-center">
+        <label htmlFor="email" className="col-sm-auto col-form-label">Local Branch</label>
+        <div className="col-sm">
+          <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+        </div>
+      </div>
+      <div className="form-group row">
+        <div className="col-sm-10 offset-sm-2">
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+      </div>
     </form>
   );
 };
